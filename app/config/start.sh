3585482a79ef6644c12686dc0446b5949edf9dca
Elisabetta Iavarone <iavarone@itis.swiss>
2022-07-25 18:42:50 +0200

    First test with tkinter

--- app/config/start.sh
+++ app/config/start.sh
@@ -6,20 +6,10 @@
 set -e
 
 INFO="INFO: [$(basename "$0")] "
+echo "$INFO: Checking if required inputs are available"
 
-while [ ! -f ${DY_SIDECAR_PATH_INPUTS}/input_1/input_image.nii.gz ]
-    do 
-    sleep 1
-    echo "$INFO: Waiting for image file in first input port"
-    done
+python3 check_inputs.py
 
-while [ ! -f ${DY_SIDECAR_PATH_INPUTS}/input_2/labels.txt ]
-    do 
-    sleep 1
-    echo "$INFO: Waiting for input labels txt file in second input port"
-    done
-
-LABELS=$(cat "${DY_SIDECAR_PATH_INPUTS}/input_2/labels.txt")
 
 echo "$INFO: Starting sct_label_utils with input file ${DY_SIDECAR_PATH_INPUTS}/input_1/input_image.nii.gz and labels ${LABEL}"
 sct_label_utils -v 1 -i ${DY_SIDECAR_PATH_INPUTS}/input_1/input_image.nii.gz -o ${DY_SIDECAR_PATH_OUTPUTS}/output_1/labels.nii.gz -create-viewer $LABELS  
